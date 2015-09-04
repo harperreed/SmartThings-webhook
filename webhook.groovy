5432e079035ef2505b2701b708f39a346dd1a786
Harper Reed <harper@nata2.org>
2015-09-04 00:06:25 -0500

    Made changes suggested by SmarthThings
    * Please add comments in the code.
    * Please add a try/catch block around the HTTP request. It may be better to check the endpoint and send the user a message if the endpoint isn't available (doesn't exist or is down).
    * Not all the inputs are used in subscribe methods. Since the callback method requires an event to send that data, these inputs don't need to be listed:
      - input "actuator", "capability.actuator", title: "Actuator", required: false, multiple: true
      - input "configuration", "capability.configuration", title: "Configuration", required: false, multiple: true
      - input "lockCodes", "capability.lockCodes", title: "Lock Codes", required: false, multiple: true
      - input "momentary", "capability.momentary", title: "Momentary", required: false, multiple: true
      - input "polling", "capability.polling", title: "Polling", required: false, multiple: true
      - input "refresh", "capability.refresh", title: "Refresh", required: false, multiple: true
      - input "speechSynthesis", "capability.speechSynthesis", title: "Speech Synthesis", required: false, multiple: true
      - input "testCapability", "capability.testCapability", title: "Test Capability", required: false, multiple: true
      - input "tone", "capability.tone", title: "Tone", required: false, multiple: true

--- webhook.groovy
+++ webhook.groovy
@@ -1,7 +1,13 @@
 /**
  * SmartThings webhooks
  *
- *  Author: Harper Reed
+ *  Author: Harper Reed (@harper - harper@nata2.org)
+ *  URL: https://github.com/harperreed/SmartThings-webhook
+ */
+ 
+/** 
+ * Let's define this dude
+ *
  */
 definition(
     name: "Webhooks",
@@ -12,21 +18,35 @@ definition(
     iconX2Url: "https://s3.amazonaws.com/kinlane-productions/bw-icons/webhooks.png"
 )
 
+/** 
+ * Collect some preferences
+ * The important piece here is the webhook URL. without that we can't do anything
+ * 
+ * The event selector is where we let the user select which sensor events they want to send to the webhook
+ *
+ */
 preferences {
+    /**
+     * This should be an accessible URL
+     * For debugging, I suggest http://requestb.in/
+     */
     section("Webhook URL"){
 		input "url", "text", title: "Webhook URL", description: "Your webhook URL", required: true
 	}
 
+	/**
+     * We will probably need to add other items as smartthings sensors grow
+     * 
+     * Feel free to add any sensors that we are missing and then submit a pull request
+     */
 	section("Choose what events you want to trigger"){
 		input "accelerationSensor", "capability.accelerationSensor", title: "Acceleration Sensor", required: false, multiple: true
-		input "actuator", "capability.actuator", title: "Actuator", required: false, multiple: true
 		input "alarm", "capability.alarm", title: "Alarm", required: false, multiple: true
 		input "battery", "capability.battery", title: "Battery", required: false, multiple: true
 		input "beacon", "capability.beacon", title: "Beacon", required: false, multiple: true
 		input "button", "capability.button", title: "Button", required: false, multiple: true
 		input "carbonMonoxideDetector", "capability.carbonMonoxideDetector", title: "Carbon Monoxide Detector", required: false, multiple: true
 		input "colorControl", "capability.colorControl", title: "Color Control", required: false, multiple: true
-		input "configuration", "capability.configuration", title: "Configuration", required: false, multiple: true
 		input "contactSensor", "capability.contactSensor", title: "Contact Sensor", required: false, multiple: true
 		input "doorControl", "capability.doorControl", title: "Door Control", required: false, multiple: true
 		input "energyMeter", "capability.energyMeter", title: "Energy Meter", required: false, multiple: true
@@ -35,15 +55,11 @@ preferences {
 		input "indicator", "capability.indicator", title: "Indicator", required: false, multiple: true
 		input "locationMode", "capability.locationMode", title: "Location Mode", required: false, multiple: true
 		input "lock", "capability.lock", title: "Lock", required: false, multiple: true
-		input "lockCodes", "capability.lockCodes", title: "Lock Codes", required: false, multiple: true
 		input "mediaController", "capability.mediaController", title: "Media Controller", required: false, multiple: true
-		input "momentary", "capability.momentary", title: "Momentary", required: false, multiple: true
 		input "motionSensor", "capability.motionSensor", title: "Motion Sensor", required: false, multiple: true
 		input "musicPlayer", "capability.musicPlayer", title: "Music Player", required: false, multiple: true
-		input "polling", "capability.polling", title: "Polling", required: false, multiple: true
 		input "powerMeter", "capability.powerMeter", title: "Power Meter", required: false, multiple: true
 		input "presenceSensor", "capability.presenceSensor", title: "Presence Sensor", required: false, multiple: true
-		input "refresh", "capability.refresh", title: "Refresh", required: false, multiple: true
 		input "relativeHumidityMeasurement", "capability.relativeHumidityMeasurement", title: "Relative Humidity Measurement", required: false, multiple: true
 		input "relaySwitch", "capability.relaySwitch", title: "Relay Switch", required: false, multiple: true
 		input "sensor", "capability.sensor", title: "Sensor", required: false, multiple: true
@@ -51,33 +67,35 @@ preferences {
 		input "sleepSensor", "capability.sleepSensor", title: "Sleep Sensor", required: false, multiple: true
 		input "smokeDetector", "capability.smokeDetector", title: "Smoke Detector", required: false, multiple: true
 		input "speechRecognition", "capability.speechRecognition", title: "Speech Recognition", required: false, multiple: true
-		input "speechSynthesis", "capability.speechSynthesis", title: "Speech Synthesis", required: false, multiple: true
 		input "stepSensor", "capability.stepSensor", title: "Step Sensor", required: false, multiple: true
 		input "switchv", "capability.switch", title: "Switch", required: false, multiple: true
 		input "switchLevel", "capability.switchLevel", title: "Switch Level", required: false, multiple: true
 		input "temperatureMeasurement", "capability.temperatureMeasurement", title: "Temperature Measurement", required: false, multiple: true
-		input "testCapability", "capability.testCapability", title: "Test Capability", required: false, multiple: true
 		input "thermostat", "capability.thermostat", title: "Thermostat", required: false, multiple: true
 		input "thermostatCoolingSetpoint", "capability.thermostatCoolingSetpoint", title: "Thermostat Cooling Setpoint", required: false, multiple: true
 		input "threeAxis", "capability.threeAxis", title: "Three Axis", required: false, multiple: true
-		input "tone", "capability.tone", title: "Tone", required: false, multiple: true
 		input "touchSensor", "capability.touchSensor", title: "TouchSensor", required: false, multiple: true
 		input "valve", "capability.valve", title: "Valve", required: false, multiple: true
 		input "waterSensor", "capability.waterSensor", title: "Water Sensor", required: false, multiple: true
-
-        
 	}
-
-    
-
 }
 
+/**
+ * Installer handler
+ * 
+ * The key piece here is the subscribeToEvents() function. This triggers the subscription of the events that matter
+ */
 def installed() {
 	log.debug "Installed with settings: ${settings}"
 	subscribeToEvents()
     initialize()
 }
 
+/**
+ * Update handler
+ *
+ * Basically the same as the installer - but it will unsubscribe everything before you subscribe again
+ */
 def updated() {
 	log.debug "Updated with settings: ${settings}"
 	unsubscribe()
@@ -85,12 +103,19 @@ def updated() {
     initialize()
 }
 
+/**
+ * Init this dude!!
+ */
 def initialize() {
 	subscribe(app, eventHandler)
-
 }
 
-
+/**
+ * Subscribe to those events
+ * 
+ * This is the meat of the app. When an event happens that we have subscribed to, we throw that event to the eventHandler function
+ * Works pretty well. 
+ */
 def subscribeToEvents() {
 	subscribe(accelerationSensor, "acceleration ", eventHandler)
 
@@ -156,18 +181,25 @@ def subscribeToEvents() {
 
 }
 
+/**
+ * EventHandler!!
+ * 
+ * This is the function that communicates externally. 
+ * There are 3 parts:
+ *  1) Create the json object
+ *  2) Create the request params object (including URL)
+ *  3) make the request to webhook URL
+ *
+ * I am forcing json because it is 2015
+ *
+ * This works pretty well
+ */
 def eventHandler(evt) {
-	/*
-	def postBody = "[date:'${evt.date}', deviceId: '${evt.deviceId}', value:'${evt.value}', source:'${evt.source}', type:'${evt.type}', name:'${evt.displayName}']"
-
-	def params = [
-  		uri: settings.url,
-  		success: successClosure,
-	  	body: postBody 
-	]
-	httpPost(params)
-	*/
     def state_changed = evt.isStateChange()
+    /**
+     * Json object
+     * I tired to include everything
+     */
 	def json_body = [
             id: evt.deviceId, 
 			date: evt.isoDate,
@@ -188,11 +220,17 @@ def eventHandler(evt) {
   		success: successClosure,
 	  	body: json_body
 	]
-	httpPostJson(json_params)
-
+    
+    try {
+		httpPostJson(json_params)
+	} catch (e) {
+    	log.error "http post failed: $e"
+	}
 }
 
+/**
+ * Called when a successful webhook post happens!
+ */
 def successClosure = { response ->
   log.debug "Request was successful, $response"
 }
-
